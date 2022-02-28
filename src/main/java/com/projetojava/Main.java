7c5bf07719abcebed2ac6355d5052eeb64092460
Victor Betoni <victorbetoni@protonmail.com>
2022-02-28 13:05:32 -0300

    First commit

---
+++ src/main/java/com/projetojava/Main.java
@@ -0,0 +1,18 @@
+package com.projetojava;
+
+/* Para rodar, digite no terminal: ./gradlew run
+ *
+ * Lembre-se: Caso você queira mudar o nome do pacote ou o nome dessa classe,
+ * você precisa também mudar a propriedade "ext.javaMainClass" no arquivo build.gradle.
+ * Caso você mude o nome do pacote ou o nome da classe com o método main e não mudar
+ * não alterá-las no arquivo build.gradle também, vai dar erro.
+ * 
+ * Divirta-se
+*/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
